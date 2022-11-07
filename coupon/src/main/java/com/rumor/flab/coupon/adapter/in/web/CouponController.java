c59c78e46326ff0893554b9464b898f225269c7b
rumor1993 <rumor1993@naver.com>
2022-11-08 01:13:48 +0900

    feature : Coupon 영속성 계층 구성

--- coupon/src/main/java/com/rumor/flab/coupon/adapter/in/web/CouponController.java
+++ coupon/src/main/java/com/rumor/flab/coupon/adapter/in/web/CouponController.java
@@ -1,19 +1,29 @@
 package com.rumor.flab.coupon.adapter.in.web;
 
-import com.rumor.flab.coupon.application.CouponService;
+import com.rumor.flab.coupon.application.GetCouponService;
+import com.rumor.flab.coupon.application.RegisterCouponService;
+import com.rumor.flab.coupon.domain.Coupon;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class CouponController {
 
-    private final CouponService couponService;
+    private final GetCouponService getCouponService;
+    private final RegisterCouponService registerCouponService;
 
     @GetMapping("/coupons")
     public String coupons() {
-        couponService.getCoupons();
+        getCouponService.getCoupons();
         return "coupon";
+    }
+
+    @PostMapping("/coupon")
+    public String registerCoupon(Coupon coupon) {
+        registerCouponService.registerCoupon(coupon);
+        return "create";
     }
 }
--- coupon/src/main/java/com/rumor/flab/coupon/adapter/out/persistence/CouponRepository.java
+++ coupon/src/main/java/com/rumor/flab/coupon/adapter/out/persistence/CouponRepository.java
@@ -1,11 +1,31 @@
 package com.rumor.flab.coupon.adapter.out.persistence;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.rumor.flab.coupon.application.port.out.LoadCouponPort;
+import com.rumor.flab.coupon.application.port.out.RegisterCouponPort;
 import com.rumor.flab.coupon.domain.Coupon;
-import org.springframework.data.jpa.repository.JpaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface CouponRepository extends JpaRepository<Coupon, Long>, LoadCouponPort {
+import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
+public class CouponRepository implements LoadCouponPort, RegisterCouponPort {
+
+    private final CouponSpringDataJpaRepository couponSpringDataJpaRepository;
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public List<Coupon> findAll() {
+        List<CouponEntity> couponEntities = couponSpringDataJpaRepository.findAll();
+        return objectMapper.convertValue(couponEntities, new TypeReference<List<Coupon>>() {});
+    }
+
+    @Override
+    public CouponEntity registerCoupon(Coupon coupon) {
+        CouponEntity couponEntity = objectMapper.convertValue(coupon, CouponEntity.class);
+        return couponSpringDataJpaRepository.save(couponEntity);
+    }
 }
