029ef792ea34d6902afd0cec7190ee57443b57ac
rumor1993 <rumor1993@naver.com>
2022-11-08 01:13:15 +0900

    feature : Coupon 등록 유즈케이스

--- coupon/src/main/java/com/rumor/flab/coupon/application/port/in/RegisterCouponUseCase.java
+++ coupon/src/main/java/com/rumor/flab/coupon/application/port/in/RegisterCouponUseCase.java
@@ -1,4 +1,8 @@
 package com.rumor.flab.coupon.application.port.in;
 
+import com.rumor.flab.coupon.adapter.out.persistence.CouponEntity;
+import com.rumor.flab.coupon.domain.Coupon;
+
 public interface RegisterCouponUseCase {
+    CouponEntity registerCoupon(Coupon coupon);
 }
