ccacfa570d6d4dadfcb72b0db42daaeb4340d87f
rumor1993 <rumor1993@naver.com>
2022-11-08 01:13:31 +0900

    feature : Coupon 조회 로직

--- coupon/src/main/java/com/rumor/flab/coupon/application/GetCouponService.java
+++ coupon/src/main/java/com/rumor/flab/coupon/application/GetCouponService.java
@@ -1,5 +1,8 @@
 package com.rumor.flab.coupon.application;
 
+import com.rumor.flab.coupon.adapter.out.persistence.CouponEntity;
+import com.rumor.flab.coupon.application.port.in.GetCouponUserCase;
+import com.rumor.flab.coupon.application.port.in.RegisterCouponUseCase;
 import com.rumor.flab.coupon.application.port.out.LoadCouponPort;
 import com.rumor.flab.coupon.domain.Coupon;
 import lombok.RequiredArgsConstructor;
@@ -9,10 +12,11 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class CouponService {
+public class GetCouponService implements GetCouponUserCase {
 
     private final LoadCouponPort loadCouponPort;
 
+    @Override
     public List<Coupon> getCoupons() {
         return loadCouponPort.findAll();
     }
