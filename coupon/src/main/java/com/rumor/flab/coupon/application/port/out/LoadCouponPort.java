268dc769a46dfe8966cbabb76af5b54eb0e3ee58
rumor1993 <rumor1993@naver.com>
2022-11-07 18:57:56 +0900

    feature : 클린아키텍처 영속성 처리

--- coupon/src/main/java/com/rumor/flab/coupon/application/port/out/LoadCouponPort.java
+++ coupon/src/main/java/com/rumor/flab/coupon/application/port/out/LoadCouponPort.java
@@ -1,4 +1,9 @@
 package com.rumor.flab.coupon.application.port.out;
 
+import com.rumor.flab.coupon.domain.Coupon;
+
+import java.util.List;
+
 public interface LoadCouponPort {
+    public List<Coupon> findAll();
 }
