3fc2d97c5d94537e53f344d229cfe3f93a4821f0
rumor1993 <rumor1993@naver.com>
2022-11-13 00:58:26 +0900

    refactor : 드러낼 필요가 없는 정보 제거

--- coupon/src/main/java/com/rumor/flab/coupon/adapter/in/web/ResponseCoupon.java
+++ coupon/src/main/java/com/rumor/flab/coupon/adapter/in/web/ResponseCoupon.java
@@ -1,5 +1,6 @@
 package com.rumor.flab.coupon.adapter.in.web;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -7,6 +8,7 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 @Getter
+@EqualsAndHashCode
 @NoArgsConstructor
 public class ResponseCoupon {
 
@@ -14,33 +16,11 @@ public class ResponseCoupon {
     private Long owner;
     private String name;
     private String image;
-    private LocalDate created;
-    private LocalDate updated;
 
-    public ResponseCoupon(Long id, Long owner, String name, String image, LocalDate created, LocalDate updated) {
+    public ResponseCoupon(Long id, Long owner, String name, String image) {
         this.id = id;
         this.owner = owner;
         this.name = name;
         this.image = image;
-        this.created = created;
-        this.updated = updated;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ResponseCoupon that = (ResponseCoupon) o;
-        return Objects.equals(id, that.id) &&
-                Objects.equals(owner, that.owner) &&
-                Objects.equals(name, that.name) &&
-                Objects.equals(image, that.image) &&
-                Objects.equals(created, that.created) &&
-                Objects.equals(updated, that.updated);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, owner, name, image, created, updated);
     }
 }
