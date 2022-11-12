23392f782c990da89090d9f4f9dad0f7af59848b
rumor1993 <rumor1993@naver.com>
2022-11-13 00:39:36 +0900

    refactor : ObjectMapper 모듈 추가
    LocalDate 직렬화가 안되는 이슈 처리를 위해 추가

--- coupon/src/main/java/com/rumor/flab/coupon/CouponConfiguration.java
+++ coupon/src/main/java/com/rumor/flab/coupon/CouponConfiguration.java
@@ -3,6 +3,7 @@ package com.rumor.flab.coupon;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,6 +14,7 @@ public class CouponConfiguration {
     public ObjectMapper objectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+        objectMapper.registerModule(new JavaTimeModule());
         return objectMapper;
     }
 }
