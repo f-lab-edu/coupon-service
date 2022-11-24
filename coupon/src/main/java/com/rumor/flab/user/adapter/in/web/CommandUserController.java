514d39353db72d8df80bbb8fdda9044f2610a9a5
rumor1993 <rumor1993@naver.com>
2022-11-24 17:34:28 +0900

    feature : change Http Method

--- coupon/src/main/java/com/rumor/flab/user/adapter/in/web/CommandUserController.java
+++ coupon/src/main/java/com/rumor/flab/user/adapter/in/web/CommandUserController.java
@@ -1,16 +1,21 @@
 package com.rumor.flab.user.adapter.in.web;
 
 import com.rumor.flab.user.application.SocialService;
+import com.rumor.flab.user.application.enums.SocialType;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/user")
+@RestController
+@RequiredArgsConstructor
 public class CommandUserController {
 
-    SocialService socialService;
+    private final SocialService socialService;
 
-    @PostMapping("/login}")
-    public void login() {
-        socialService.login();
+    // NOTE: 각 소셜마다 로그인시 주어지는 파라미터가 다를 수 있음!
+    @PostMapping("/user/google/login")
+    public void login(@RequestParam String credential) {
+        socialService.login(SocialType.GOOGLE, credential);
     }
 }
