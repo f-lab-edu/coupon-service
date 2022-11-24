2c533aee63077077d1a53b9e65db62c7cb5552c6
rumor1993 <rumor1993@naver.com>
2022-11-24 12:07:25 +0900

    feature : 유저 서비스 추가
    로그인 로직 처리

---
+++ coupon/src/main/java/com/rumor/flab/user/application/SocialService.java
@@ -0,0 +1,10 @@
+package com.rumor.flab.user.application;
+
+import com.rumor.flab.user.application.handler.SocialProviderHandler;
+
+public class SocialService {
+
+    public void login() {
+        SocialProviderHandler handler;
+    }
+}
