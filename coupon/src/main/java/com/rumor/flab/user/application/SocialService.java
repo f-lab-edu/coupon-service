7e9e651c1bc099f60b846d769da909650e431773
rumor1993 <rumor1993@naver.com>
2022-11-24 18:44:59 +0900

    feature : User 등록 로직 추가

--- coupon/src/main/java/com/rumor/flab/user/application/SocialService.java
+++ coupon/src/main/java/com/rumor/flab/user/application/SocialService.java
@@ -4,6 +4,8 @@ import com.rumor.flab.user.application.dto.SocialUser;
 import com.rumor.flab.user.application.enums.SocialType;
 import com.rumor.flab.user.application.factory.SocialProviderHandlerFactory;
 import com.rumor.flab.user.application.handler.SocialProviderHandler;
+import com.rumor.flab.user.application.port.out.RegisterUserPort;
+import com.rumor.flab.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +13,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class SocialService {
 
+    private final RegisterUserPort registerUserPort;
     private final SocialProviderHandlerFactory socialProviderHandlerFactory;
 
     public void login(SocialType socialType, String credential) {
         SocialProviderHandler handler = socialProviderHandlerFactory.findHandler(socialType);
         SocialUser socialUser = handler.oauthLogin(credential);
+
+        User user = new User(socialUser.getEmail(), "", "");
+        registerUserPort.registerUser(user);
     }
 }
--- coupon/src/main/java/com/rumor/flab/user/application/dto/SocialUser.java
+++ coupon/src/main/java/com/rumor/flab/user/application/dto/SocialUser.java
@@ -1,4 +1,5 @@
 package com.rumor.flab.user.application.dto;
 
 public interface SocialUser {
+    public String getEmail();
 }
