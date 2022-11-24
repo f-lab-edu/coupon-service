a7b35b6e5c2edf1c384772bafb7ee0c94f5fcbcf
rumor1993 <rumor1993@naver.com>
2022-11-24 17:37:42 +0900

    feature : 핸들러 추출 로직 및 로그인 로직 처리

--- coupon/src/main/java/com/rumor/flab/user/application/SocialService.java
+++ coupon/src/main/java/com/rumor/flab/user/application/SocialService.java
@@ -1,10 +1,19 @@
 package com.rumor.flab.user.application;
 
+import com.rumor.flab.user.application.enums.SocialType;
+import com.rumor.flab.user.application.factory.SocialProviderHandlerFactory;
 import com.rumor.flab.user.application.handler.SocialProviderHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class SocialService {
 
-    public void login() {
-        SocialProviderHandler handler;
+    SocialProviderHandlerFactory socialProviderHandlerFactory;
+
+    public void login(SocialType socialType, String credential) {
+        SocialProviderHandler handler = socialProviderHandlerFactory.findHandler(socialType);
+        handler.oauthLogin(credential);
     }
 }
